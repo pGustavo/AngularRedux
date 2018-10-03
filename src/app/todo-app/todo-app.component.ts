58dee92e65159a9606dc59ad8612f2a131089b30
Gustavo Passos <gustavo.passos@wintrust.tech>
2018-10-03 12:07:34 +0100

    Todo APP

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+interface TodoItem {
+  name: String;
+  done: Boolean;
+}
 @Component({
   selector: 'app-todo-app',
   templateUrl: './todo-app.component.html',
   styleUrls: ['./todo-app.component.css']
 })
+
 export class TodoAppComponent implements OnInit {
 
-  constructor() { }
+  newTodo: String = '';
+  show = false;
+
+  list: Array<TodoItem> = [
+    {name: 'clean room', done: false},
+    {name: 'make pancakes', done: false},
+    {name: 'spend 3 hours on reddit', done: true}
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  addTodo() {
+    console.log('this.newTodo', this.newTodo);
+
+    this.list.push({
+      name: this.newTodo,
+      done: false
+    });
+
+    this.newTodo = '';
+
+  }
+  toggle() {
+    this.show = !this.show;
+  }
 }
