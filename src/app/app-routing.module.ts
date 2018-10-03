d6ebd3e266f7f86a0a7efe8c9b085d1a1c3817aa
Gustavo Passos <gustavo.passos@wintrust.tech>
2018-10-03 10:50:56 +0100

    step 6 with style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{                                       // add
+  path: '',                                                   // add
+  loadChildren: 'app/home/home.module#HomeModule'             // add
+}, {                                                            // add
+  path: 'about',                                              // add
+  loadChildren: 'app/about/about.module#AboutModule'          // add
+}, {                                                            // add
+  path: 'todo',                                               // add
+  loadChildren: 'app/todo-app/todo-app.module#TodoAppModule'  // add
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
